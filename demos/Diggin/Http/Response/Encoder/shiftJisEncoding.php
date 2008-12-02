24eae3955a6e14fed0f3f9eeed5ef01d63dad4be
sasezaki <sasezaki@224e63c0-5457-0410-989b-907d9cf58824>
2008-12-02 15:31:09 +0000

    Response_Encoder用 demo追加
    git-svn-id: http://svn.openpear.org/Diggin_Scraper_Adapter_Htmlscraping/trunk@157 224e63c0-5457-0410-989b-907d9cf58824

---
+++ demos/Diggin/Http/Response/Encoder/shiftJisEncoding.php
@@ -0,0 +1,18 @@
+<?php
+require_once 'Diggin/Http/Response/Encoder.php';
+require_once 'HTTP/Request2.php';
+
+$req = new HTTP_Request2();
+$req->setUrl('http://ugnews.net/');
+$response = $req->send();
+print_r($response->getHeader('content-type'));
+//$req->getBody();
+
+echo 'before encoding', PHP_EOL;
+print_r(mb_substr(strip_tags($response->getBody()), 0, 200));
+
+list($body) = Diggin_Http_Response_Encoder::encode($response->getBody(),
+                                     $response->getHeader('content-type'));
+                                     
+echo PHP_EOL, 'after encoding', PHP_EOL;
+print_r(mb_substr(strip_tags($body), 0, 200));
