347690870a6b482dbb6201aee671dcfff00ab59a
sasezaki <sasezaki@224e63c0-5457-0410-989b-907d9cf58824>
2008-12-05 18:29:17 +0000

    demo修正
    git-svn-id: http://svn.openpear.org/Diggin_Scraper_Adapter_Htmlscraping/trunk@181 224e63c0-5457-0410-989b-907d9cf58824

--- demos/Diggin/Http/Response/Encoder/shiftJisEncoding.php
+++ demos/Diggin/Http/Response/Encoder/shiftJisEncoding.php
@@ -5,14 +5,6 @@ require_once 'HTTP/Request2.php';
 $req = new HTTP_Request2();
 $req->setUrl('http://ugnews.net/');
 $response = $req->send();
-print_r($response->getHeader('content-type'));
-//$req->getBody();
+$encoded = Diggin_Http_Response_Encoder::encodeFromObject($response);
 
-echo 'before encoding', PHP_EOL;
-print_r(mb_substr(strip_tags($response->getBody()), 0, 200));
-
-$body = Diggin_Http_Response_Encoder::encode($response->getBody(),
-                                     $response->getHeader('content-type'), 'UTF-8');
-                                     
-echo PHP_EOL, 'after encoding', PHP_EOL;
-print_r(mb_substr(strip_tags($body), 0, 200));
+var_dump(strip_tags($encoded)); //utf-8
