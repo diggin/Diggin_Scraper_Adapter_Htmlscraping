9fd32709e005f06ded95f5e26ece1622735e5ffa
sasezaki <sasezaki@224e63c0-5457-0410-989b-907d9cf58824>
2008-12-11 12:29:54 +0000

    git-svn-id: http://svn.openpear.org/Diggin_Scraper_Adapter_Htmlscraping/trunk@199 224e63c0-5457-0410-989b-907d9cf58824

--- demos/Diggin/Http/Response/Encoder/utf8_From_eucjp.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'Diggin/Http/Response/Encoder.php';
-require_once 'HTTP/Request2.php';
-
-$req = new HTTP_Request2();
-$req->setUrl('http://d.hatena.ne.jp/sasezaki/');
-$encoded = Diggin_Http_Response_Encoder::encodeFromObject($req->send());
-
-var_dump(strip_tags($encoded));  //utf-8
