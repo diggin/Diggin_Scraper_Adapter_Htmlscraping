5de1ba8efdce4beda7291ac47d8e9c3871d48603
sasezaki <sasezaki@224e63c0-5457-0410-989b-907d9cf58824>
2008-12-11 14:15:21 +0000

    Exception名間違ってたの修正とか
    git-svn-id: http://svn.openpear.org/Diggin_Scraper_Adapter_Htmlscraping/trunk@202 224e63c0-5457-0410-989b-907d9cf58824

--- library/Diggin/Http/Response/Exception.php
+++ library/Diggin/Http/Response/Exception.php
@@ -9,15 +9,14 @@
  * 
  * @category   Diggin
  * @package    Diggin_Http
- * @subpackage Response_Encoder
  * @copyright  2006-2008 sasezaki (http://diggin.musicrider.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
 
 /**
- * @see Diggin_Http_Response_Exception
+ * @see Diggin_Http_Exception
  */
-require_once 'Diggin/Http/Response/Exception.php';
+require_once 'Diggin/Http/Exception.php';
 
-class Diggin_Http_Response_Encoding_Exception extends Diggin_Http_Response_Exception
+class Diggin_Http_Response_Exception extends Diggin_Http_Exception
 {}
